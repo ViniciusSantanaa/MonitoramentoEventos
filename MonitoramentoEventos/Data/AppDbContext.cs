7bc57f1ba4fdabac3ff1fe4d24cd96d55abf77fd
Vinicius <viniciussantanna2005@gmail.com>
2025-06-08 23:30:16 -0300

    Adicionar arquivos de projeto.

---
+++ MonitoramentoEventos/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MonitoramentoEventos.Models;
+
+namespace MonitoramentoEventos.Data 
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Alerta> Alertas { get; set; }
+        public DbSet<LocalMonitorado> LocaisMonitorados { get; set; }
+    }
+}
---
+++ MonitoramentoEventos/Models/Alerta.cs
@@ -0,0 +1,11 @@
+﻿namespace MonitoramentoEventos.Models
+{
+    public class Alerta
+    {
+        public int Id { get; set; }
+        public string Mensagem { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
